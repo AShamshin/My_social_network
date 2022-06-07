41833c5ef3ba3079707835fad79c1bd79b32feb5
AShamshin <albert.shamshin@mail.ru>
2022-06-07 14:48:16 +0300

    commit with error

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,18 +1,20 @@
-import nature from '../photo/nature.jpg';
+import s from './Profile.module.css';
+
+import nature from '.../photo/nature.jpg';
 
 const Profile = () => {
   return (
-    <div className='content'>
+    <div className={s.content}>
       <div>
         <img src={nature} alt='' />
       </div>
       <div> ava + description </div>
       <div>
         My posts <div> Nev post </div>
-        <div>
-          <div> post 1 </div>
+        <div className={s.posts}>
+          <div className={s.item}> post 1 </div>
 
-          <div> post 2</div>
+          <div className={s.item}> post 2</div>
         </div>
       </div>
     </div>
